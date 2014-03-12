181b412d267d92e30e1b5131c3df63a78be1ceb3
sanjay <sanjay@sanjay.(none)>
2014-03-12 21:42:49 +0530

    Ticket mailer status and priority issue fix

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,4 +1,6 @@
 class TicketMailer < ActionMailer::Base
+  include ApplicationHelper
+  
   default from: "from@example.com"
   
   # send ticket link
@@ -6,6 +8,8 @@ class TicketMailer < ActionMailer::Base
     @ticket = opts[:ticket]
     @email = opts[:email]
     @password = opts[:password]
+    @priority = ticket_priority_hash[@ticket.priority_id.to_s]
+    @status = ticket_status_hash[@ticket.status_id.to_s]
     mail(:to => "#{email}", :subject => "SupportEngine - Ticket##{@ticket.ticket_secret} #{@ticket.subject}")
   end
   
@@ -13,6 +17,8 @@ class TicketMailer < ActionMailer::Base
     @ticket = opts[:ticket]
     @email = opts[:email]
     @password = opts[:password]
+    @priority = ticket_priority_hash[@ticket.priority_id.to_s]
+    @status = ticket_status_hash[@ticket.status_id.to_s]
     mail(:to => "#{email}", :subject => "SupportEngine - Ticket##{@ticket.ticket_secret} #{@ticket.subject}")
   end  
   
